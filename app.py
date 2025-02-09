b1cc6fe86c62c784caad4855e28ad1dc639ae4c8
Mr Cat <142336149+Bipre2466@users.noreply.github.com>
2025-02-09 15:07:43 +0100

    added dockerfile and the main sourcecode

---
+++ app.py
@@ -0,0 +1,3 @@
+print("hello World from Morgan")
+print("------------------------")
+print("This is the first version of my docker image ! You can wiew all the sourcecode on my GitHub profile. (bipre2466)")
